5370f9d2726b8b7f4de2c197249934c1022ed0a0
Jacob Stimes <jacob.stimes@gmail.com>
2019-12-19 22:40:59 -0800

    game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,10 +23,21 @@ interface Shape {
   velocity: number[];
 }
 
+interface Point {
+  x: number;
+  y: number;
+}
+
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 600;
 
-const vsSource = `
+const SQUARE_RADIUS = 1.0;
+const X_BOUNDS = 5.6;
+const Y_BOUNDS = 4.0;
+const Z_BOUNDS = 5.0;
+const SHAPE_Z = -12.0;
+
+const VERTEX_SHADER_SOURCE = `
   attribute vec4 aVertexPosition;
 
   uniform mat4 uModelViewMatrix;
@@ -37,7 +48,7 @@ const vsSource = `
   }
 `;
 
-const fsSource = `
+const FRAGMENT_SHADER_SOURCE = `
   void main() {
     gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
   }
@@ -50,15 +61,21 @@ const fsSource = `
 })
 export class AppComponent {
   title = 'JacobX';
+  canvas: HTMLCanvasElement;
   gl: WebGLRenderingContext;
   program: Program;
   buffers: Buffers;
 
+  shapes: Shape[] = [];
+
   ngOnInit() {
     const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    this.canvas = canvas;
     canvas.setAttribute('width', `${CANVAS_WIDTH}`);
     canvas.setAttribute('height', `${CANVAS_HEIGHT}`);
-    canvas.onclick = this.onClick;
+    canvas.addEventListener('mousedown', this.onMouseDown);
+    canvas.addEventListener('mousemove', this.onMouseMove);
+    canvas.addEventListener('mouseup', this.onMouseUp);
     
     const gl = canvas.getContext("webgl");
     this.gl = gl;
@@ -74,7 +91,7 @@ export class AppComponent {
     // Clear the color buffer with specified clear color
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    const shaderProgram = this.initShaderProgram(gl, vsSource, fsSource);
+    const shaderProgram = this.initShaderProgram(gl, VERTEX_SHADER_SOURCE, FRAGMENT_SHADER_SOURCE);
     this.program = {
       program: shaderProgram,
       attribLocations: {
@@ -86,8 +103,6 @@ export class AppComponent {
       },
     };
     this.buffers = this.initBuffers(gl);
-
-    this.initShapes(6);
     this.gameLoop(0);
   }
 
@@ -101,38 +116,63 @@ export class AppComponent {
     });
   }
 
-  onClick = (e: MouseEvent) => {
-    console.log('clickity lickity');
-    const x = e.clientX;
-    const y = CANVAS_HEIGHT - e.clientY;
-    console.log(x);
-    console.log(y);
-    const glX = x * ((this.X_BOUNDS * 2) / CANVAS_WIDTH) - this.X_BOUNDS;
-    const glY = y * ((this.Y_BOUNDS * 2) / CANVAS_HEIGHT) - this.Y_BOUNDS;
-    console.log('shape coords: ' + glX + ', ' + glY);
+  isShapeActive = false;
+
+  onMouseDown = (e: MouseEvent) => {
+    if (this.isShapeActive) {
+      return;
+    }
+    const glCoords = this.getGlCoords(e);
+    this.isShapeActive = true;
     this.shapes.push({
       velocity: this.getRandomVelocity(),
-      translation: [glX, glY, this.shapeZ],
-    })
+      translation: [glCoords.x, glCoords.y, SHAPE_Z],
+    });
   };
 
-  shapeZ = -12.0;
-  shapes: Shape[] = [];
-  INITIAL_VELOCITY = [.01, .01, 0.0];
-  initShapes(total: number) {
-    for (let i=0; i<total; i++) {
-      const theta = Math.random() * 2 * Math.PI;
-      const xVel = Math.cos(theta) * .05
-      const yVel = Math.sin(theta) * .05;
-      this.shapes.push({
-        translation: [
-          (Math.random() * (this.X_BOUNDS * 2)) - this.X_BOUNDS, 
-          (Math.random() * (this.Y_BOUNDS * 2)) - this.Y_BOUNDS, 
-          this.shapeZ,
-        ],
-        velocity: this.getRandomVelocity(),
-      });
+  private getGlCoords(e: MouseEvent): Point {
+    const x = e.clientX;
+    const y = CANVAS_HEIGHT - e.clientY;
+    const glX = x * ((X_BOUNDS * 2) / CANVAS_WIDTH) - X_BOUNDS;
+    const glY = y * ((Y_BOUNDS * 2) / CANVAS_HEIGHT) - Y_BOUNDS;
+    return {x: glX, y: glY};
+  }
+
+  onMouseMove = (e: MouseEvent) => {
+    if (!this.isShapeActive) {
+      return;
     }
+    const activeShape = this.shapes[this.shapes.length - 1];
+    const glCoords = this.getGlCoords(e);
+    activeShape.translation = [glCoords.x, glCoords.y, SHAPE_Z];
+  }
+
+  onMouseUp = (e: MouseEvent) => {
+    if (!this.isShapeActive) {
+      return;
+    }
+
+    // Check for overlap with existing shape - if so, defeat.
+    const activeShape = this.shapes[this.shapes.length - 1];
+    for (let i=0; i<this.shapes.length - 1; i++) {
+      if (this.shapesOverlap(activeShape, this.shapes[i])) {
+        alert("You lost");
+        activeShape.velocity = [0, 0, 0];
+        this.shapes[i].velocity = [0, 0, 0];
+        this.canvas.removeEventListener('mousedown', this.onMouseDown);
+        this.canvas.removeEventListener('mousemove', this.onMouseMove);
+        this.canvas.removeEventListener('mouseup', this.onMouseUp);
+        return;
+      }
+    }
+    document.getElementById('score').innerHTML = `Score ${this.shapes.length}`;
+
+    this.isShapeActive = false;
+  }
+
+  private shapesOverlap(shapeA: Shape, shapeB: Shape): boolean {
+    return Math.abs(shapeA.translation[0] - shapeB.translation[0]) < SQUARE_RADIUS * 2
+        && Math.abs(shapeA.translation[1] - shapeB.translation[1]) < SQUARE_RADIUS * 2;
   }
 
   private getRandomVelocity(): number[] {
@@ -141,20 +181,18 @@ export class AppComponent {
     const yVel = Math.sin(theta) * .05;
     return [xVel, yVel, 0.0];
   }
-
-  readonly X_BOUNDS = 5.6;
-  readonly Y_BOUNDS = 4.0;
-  readonly Z_BOUNDS = 5.0;
+  
   update(elapsedTime: number) {
-    for (let i=0; i<this.shapes.length; i++) {
+    const stop = this.isShapeActive ? this.shapes.length - 1 : this.shapes.length;
+    for (let i=0; i<stop; i++) {
       const shape: Shape = this.shapes[i];
-      if (shape.translation[0] < -this.X_BOUNDS || shape.translation[0] > this.X_BOUNDS) {
+      if (shape.translation[0] < -X_BOUNDS || shape.translation[0] > X_BOUNDS) {
         shape.velocity[0] = -shape.velocity[0];
       }
-      if (shape.translation[1] < -this.Y_BOUNDS || shape.translation[1] > this.Y_BOUNDS) {
+      if (shape.translation[1] < -Y_BOUNDS || shape.translation[1] > Y_BOUNDS) {
         shape.velocity[1] = -shape.velocity[1];
       }
-      if (shape.translation[2] < -this.Z_BOUNDS || shape.translation[2] > this.Z_BOUNDS) {
+      if (shape.translation[2] < -Z_BOUNDS || shape.translation[2] > Z_BOUNDS) {
         shape.velocity[2] = -shape.velocity[2];
       }
       shape.translation[0] += shape.velocity[0];
@@ -256,10 +294,10 @@ export class AppComponent {
   
     // Now create an array of positions for the square.
     const positions = [
-      -1.0,  1.0,
-       1.0,  1.0,
-      -1.0, -1.0,
-       1.0, -1.0,
+      -SQUARE_RADIUS,  SQUARE_RADIUS,
+      SQUARE_RADIUS,  SQUARE_RADIUS,
+      -SQUARE_RADIUS, -SQUARE_RADIUS,
+      SQUARE_RADIUS, -SQUARE_RADIUS,
     ];
   
     // Now pass the list of positions into WebGL to build the
