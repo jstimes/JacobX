39de0f23d8e2cc0bdf7b34239c663c5017790d51
Jacob Stimes <jacob.stimes@gmail.com>
2019-12-19 22:59:53 -0800

    colors & rotation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import * as glm from './gl-matrix.js'
 
 interface AttribLocations {
   vertexPosition: number;
+  vertexColor: number;
 }
 interface UniformLocations {
   projectionMatrix: WebGLUniformLocation;
@@ -16,11 +17,14 @@ interface Program {
 
 interface Buffers {
   position: WebGLBuffer;
+  color: WebGLBuffer;
 }
 
 interface Shape {
   translation: number[];
   velocity: number[];
+  rotation: number;
+  rotationSpeed: number;
 }
 
 interface Point {
@@ -39,18 +43,24 @@ const SHAPE_Z = -12.0;
 
 const VERTEX_SHADER_SOURCE = `
   attribute vec4 aVertexPosition;
+  attribute vec4 aVertexColor;
 
   uniform mat4 uModelViewMatrix;
   uniform mat4 uProjectionMatrix;
 
+  varying lowp vec4 vColor;
+
   void main() {
     gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    vColor = aVertexColor;
   }
 `;
 
 const FRAGMENT_SHADER_SOURCE = `
+  varying lowp vec4 vColor;
+
   void main() {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    gl_FragColor = vColor;
   }
 `;
 
@@ -66,7 +76,9 @@ export class AppComponent {
   program: Program;
   buffers: Buffers;
 
-  shapes: Shape[] = [];
+  private shapes: Shape[] = [];
+  private isShapeActive = false;
+  private lastTime: number;
 
   ngOnInit() {
     const canvas = document.getElementById('canvas') as HTMLCanvasElement;
@@ -77,7 +89,7 @@ export class AppComponent {
     canvas.addEventListener('mousemove', this.onMouseMove);
     canvas.addEventListener('mouseup', this.onMouseUp);
     
-    const gl = canvas.getContext("webgl");
+    const gl = canvas.getContext('webgl');
     this.gl = gl;
   
     // Only continue if WebGL is available and working
@@ -96,6 +108,7 @@ export class AppComponent {
       program: shaderProgram,
       attribLocations: {
         vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+        vertexColor: gl.getAttribLocation(shaderProgram, 'aVertexColor'),
       },
       uniformLocations: {
         projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
@@ -106,83 +119,18 @@ export class AppComponent {
     this.gameLoop(0);
   }
 
-  lastTime: number;
-  gameLoop(elapsedTime: number) {
-    this.update(elapsedTime);
+  gameLoop(now: number) {
+    const elapsedMs = now - this.lastTime;
+    this.lastTime = now;
+    this.update(elapsedMs);
     this.render(this.gl, this.program, this.buffers);
 
     window.requestAnimationFrame((elapsedTime: number) => {
       this.gameLoop(elapsedTime);
     });
   }
-
-  isShapeActive = false;
-
-  onMouseDown = (e: MouseEvent) => {
-    if (this.isShapeActive) {
-      return;
-    }
-    const glCoords = this.getGlCoords(e);
-    this.isShapeActive = true;
-    this.shapes.push({
-      velocity: this.getRandomVelocity(),
-      translation: [glCoords.x, glCoords.y, SHAPE_Z],
-    });
-  };
-
-  private getGlCoords(e: MouseEvent): Point {
-    const x = e.clientX;
-    const y = CANVAS_HEIGHT - e.clientY;
-    const glX = x * ((X_BOUNDS * 2) / CANVAS_WIDTH) - X_BOUNDS;
-    const glY = y * ((Y_BOUNDS * 2) / CANVAS_HEIGHT) - Y_BOUNDS;
-    return {x: glX, y: glY};
-  }
-
-  onMouseMove = (e: MouseEvent) => {
-    if (!this.isShapeActive) {
-      return;
-    }
-    const activeShape = this.shapes[this.shapes.length - 1];
-    const glCoords = this.getGlCoords(e);
-    activeShape.translation = [glCoords.x, glCoords.y, SHAPE_Z];
-  }
-
-  onMouseUp = (e: MouseEvent) => {
-    if (!this.isShapeActive) {
-      return;
-    }
-
-    // Check for overlap with existing shape - if so, defeat.
-    const activeShape = this.shapes[this.shapes.length - 1];
-    for (let i=0; i<this.shapes.length - 1; i++) {
-      if (this.shapesOverlap(activeShape, this.shapes[i])) {
-        alert("You lost");
-        activeShape.velocity = [0, 0, 0];
-        this.shapes[i].velocity = [0, 0, 0];
-        this.canvas.removeEventListener('mousedown', this.onMouseDown);
-        this.canvas.removeEventListener('mousemove', this.onMouseMove);
-        this.canvas.removeEventListener('mouseup', this.onMouseUp);
-        return;
-      }
-    }
-    document.getElementById('score').innerHTML = `Score ${this.shapes.length}`;
-
-    this.isShapeActive = false;
-  }
-
-  private shapesOverlap(shapeA: Shape, shapeB: Shape): boolean {
-    return Math.abs(shapeA.translation[0] - shapeB.translation[0]) < SQUARE_RADIUS * 2
-        && Math.abs(shapeA.translation[1] - shapeB.translation[1]) < SQUARE_RADIUS * 2;
-  }
-
-  private getRandomVelocity(): number[] {
-    const theta = Math.random() * 2 * Math.PI;
-    const xVel = Math.cos(theta) * .05
-    const yVel = Math.sin(theta) * .05;
-    return [xVel, yVel, 0.0];
-  }
   
-  update(elapsedTime: number) {
+  update(elapsedMs: number) {
     const stop = this.isShapeActive ? this.shapes.length - 1 : this.shapes.length;
     for (let i=0; i<stop; i++) {
       const shape: Shape = this.shapes[i];
@@ -198,6 +146,8 @@ export class AppComponent {
       shape.translation[0] += shape.velocity[0];
       shape.translation[1] += shape.velocity[1];
       shape.translation[2] += shape.velocity[2];
+
+      shape.rotation += elapsedMs / 1000 * shape.rotationSpeed;
     }
   }
 
@@ -241,6 +191,11 @@ export class AppComponent {
       glm.mat4.translate(modelViewMatrix,     // destination matrix
                     modelViewMatrix,     // matrix to translate
                     this.shapes[i].translation);  // amount to translate
+
+      glm.mat4.rotate(modelViewMatrix,  // destination matrix
+                      modelViewMatrix,  // matrix to rotate
+                      this.shapes[i].rotation,   // amount to rotate in radians
+                    [0, 0, 1]);       // axis to rotate around
     
       // Tell WebGL how to pull out the positions from the position
       // buffer into the vertexPosition attribute.
@@ -262,6 +217,26 @@ export class AppComponent {
         gl.enableVertexAttribArray(
             programInfo.attribLocations.vertexPosition);
       }
+
+      // Tell WebGL how to pull out the colors from the color buffer
+      // into the vertexColor attribute.
+      {
+        const numComponents = 4;
+        const type = gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
+        gl.vertexAttribPointer(
+            programInfo.attribLocations.vertexColor,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset);
+        gl.enableVertexAttribArray(
+            programInfo.attribLocations.vertexColor);
+      }
     
       // Tell WebGL to use our program when drawing
       gl.useProgram(programInfo.program);
@@ -282,6 +257,54 @@ export class AppComponent {
         gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
       }
     }
+  }
+
+  private onMouseDown = (e: MouseEvent) => {
+    if (this.isShapeActive) {
+      return;
+    }
+    const glCoords = getGlCoords(e);
+    this.isShapeActive = true;
+    this.shapes.push({
+      velocity: getRandomVelocity(),
+      translation: [glCoords.x, glCoords.y, SHAPE_Z],
+      rotation: Math.random() * Math.PI * 2,
+      rotationSpeed: 1.0,
+    });
+  };
+
+  private onMouseMove = (e: MouseEvent) => {
+    if (!this.isShapeActive) {
+      return;
+    }
+    const activeShape = this.shapes[this.shapes.length - 1];
+    const glCoords = getGlCoords(e);
+    activeShape.translation = [glCoords.x, glCoords.y, SHAPE_Z];
+  }
+
+  private onMouseUp = (e: MouseEvent) => {
+    if (!this.isShapeActive) {
+      return;
+    }
+
+    // Check for overlap with existing shape - if so, defeat.
+    const activeShape = this.shapes[this.shapes.length - 1];
+    for (let i=0; i<this.shapes.length - 1; i++) {
+      if (shapesOverlap(activeShape, this.shapes[i])) {
+        alert("You lost");
+        activeShape.velocity = [0, 0, 0];
+        this.shapes[i].velocity = [0, 0, 0];
+        this.shapes[i].rotationSpeed = 0.0;
+        activeShape.rotationSpeed = 0.0;
+        this.canvas.removeEventListener('mousedown', this.onMouseDown);
+        this.canvas.removeEventListener('mousemove', this.onMouseMove);
+        this.canvas.removeEventListener('mouseup', this.onMouseUp);
+        return;
+      }
+    }
+    document.getElementById('score').innerHTML = `Score ${this.shapes.length}`;
+
+    this.isShapeActive = false;
   }
 
   private initBuffers(gl: WebGLRenderingContext): Buffers {
@@ -306,9 +329,21 @@ export class AppComponent {
     gl.bufferData(gl.ARRAY_BUFFER,
                   new Float32Array(positions),
                   gl.STATIC_DRAW);
+
+    const colors = [
+      1.0,  1.0,  1.0,  1.0,    // white
+      1.0,  0.0,  0.0,  1.0,    // red
+      0.0,  1.0,  0.0,  1.0,    // green
+      0.0,  0.0,  1.0,  1.0,    // blue
+    ];
+  
+    const colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
   
     return {
       position: positionBuffer,
+      color: colorBuffer,
     };
   }
 
@@ -348,4 +383,24 @@ export class AppComponent {
   
     return shader;
   }
+}
+
+function shapesOverlap(shapeA: Shape, shapeB: Shape): boolean {
+  return Math.abs(shapeA.translation[0] - shapeB.translation[0]) < SQUARE_RADIUS * 2
+      && Math.abs(shapeA.translation[1] - shapeB.translation[1]) < SQUARE_RADIUS * 2;
+}
+
+function getGlCoords(e: MouseEvent): Point {
+  const x = e.clientX;
+  const y = CANVAS_HEIGHT - e.clientY;
+  const glX = x * ((X_BOUNDS * 2) / CANVAS_WIDTH) - X_BOUNDS;
+  const glY = y * ((Y_BOUNDS * 2) / CANVAS_HEIGHT) - Y_BOUNDS;
+  return {x: glX, y: glY};
+}
+
+function getRandomVelocity(): number[] {
+  const theta = Math.random() * 2 * Math.PI;
+  const xVel = Math.cos(theta) * .05
+  const yVel = Math.sin(theta) * .05;
+  return [xVel, yVel, 0.0];
 }
