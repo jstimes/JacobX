a22f4a36e706afa0b34b52930e24cc054cf03064
Jacob Stimes <jacob.stimes@gmail.com>
2019-12-20 12:14:37 -0800

    3D

--- src/app/gl_2d.ts
+++ src/app/gl_2d.ts
@@ -18,7 +18,6 @@ interface Program {
 interface Buffers {
   position: WebGLBuffer;
   color: WebGLBuffer;
-  indices: WebGLBuffer;
 }
 
 interface Shape {
@@ -26,7 +25,6 @@ interface Shape {
   velocity: number[];
   rotation: number;
   rotationSpeed: number;
-  rotationAxis: number[];
 }
 
 interface Point {
@@ -40,8 +38,7 @@ const CANVAS_HEIGHT = 600;
 const SQUARE_RADIUS = 1.0;
 const X_BOUNDS = 5.6;
 const Y_BOUNDS = 4.0;
-const Z_MIN = -18.0;
-const Z_MAX = -12.0;
+const Z_BOUNDS = 5.0;
 const SHAPE_Z = -12.0;
 
 const VERTEX_SHADER_SOURCE = `
@@ -143,7 +140,7 @@ export class AppComponent {
       if (shape.translation[1] < -Y_BOUNDS || shape.translation[1] > Y_BOUNDS) {
         shape.velocity[1] = -shape.velocity[1];
       }
-      if (shape.translation[2] < -Z_MIN || shape.translation[2] > Z_MAX) {
+      if (shape.translation[2] < -Z_BOUNDS || shape.translation[2] > Z_BOUNDS) {
         shape.velocity[2] = -shape.velocity[2];
       }
       shape.translation[0] += shape.velocity[0];
@@ -195,17 +192,15 @@ export class AppComponent {
                     modelViewMatrix,     // matrix to translate
                     this.shapes[i].translation);  // amount to translate
 
-      // glm.mat4.rotate(modelViewMatrix,  // destination matrix
-      //                 modelViewMatrix,  // matrix to rotate
-      //                 this.shapes[i].rotation,   // amount to rotate in radians
-      //               [0, 0, 1]);       // axis to rotate around
-
-      glm.mat4.rotate(modelViewMatrix, modelViewMatrix, this.shapes[i].rotation, this.shapes[i].rotationAxis);
+      glm.mat4.rotate(modelViewMatrix,  // destination matrix
+                      modelViewMatrix,  // matrix to rotate
+                      this.shapes[i].rotation,   // amount to rotate in radians
+                    [0, 0, 1]);       // axis to rotate around
     
       // Tell WebGL how to pull out the positions from the position
       // buffer into the vertexPosition attribute.
       {
-        const numComponents = 3;  // pull out 2 values per iteration
+        const numComponents = 2;  // pull out 2 values per iteration
         const type = gl.FLOAT;    // the data in the buffer is 32bit floats
         const normalize = false;  // don't normalize
         const stride = 0;         // how many bytes to get from one set of values to the next
@@ -242,9 +237,6 @@ export class AppComponent {
         gl.enableVertexAttribArray(
             programInfo.attribLocations.vertexColor);
       }
-
-      // Tell WebGL which indices to use to index the vertices
-      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
     
       // Tell WebGL to use our program when drawing
       gl.useProgram(programInfo.program);
@@ -260,10 +252,9 @@ export class AppComponent {
           modelViewMatrix);
     
       {
-        const vertexCount = 36;
-        const type = gl.UNSIGNED_SHORT;
         const offset = 0;
-        gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+        const vertexCount = 4;
+        gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
       }
     }
   }
@@ -279,7 +270,6 @@ export class AppComponent {
       translation: [glCoords.x, glCoords.y, SHAPE_Z],
       rotation: Math.random() * Math.PI * 2,
       rotationSpeed: 1.0,
-      rotationAxis: getRandomRotationAxis(),
     });
   };
 
@@ -326,48 +316,11 @@ export class AppComponent {
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   
     // Now create an array of positions for the square.
-    // const positions = [
-    //   -SQUARE_RADIUS,  SQUARE_RADIUS,
-    //   SQUARE_RADIUS,  SQUARE_RADIUS,
-    //   -SQUARE_RADIUS, -SQUARE_RADIUS,
-    //   SQUARE_RADIUS, -SQUARE_RADIUS,
-    // ];
     const positions = [
-      // Front face
-      -1.0, -1.0,  1.0,
-       1.0, -1.0,  1.0,
-       1.0,  1.0,  1.0,
-      -1.0,  1.0,  1.0,
-      
-      // Back face
-      -1.0, -1.0, -1.0,
-      -1.0,  1.0, -1.0,
-       1.0,  1.0, -1.0,
-       1.0, -1.0, -1.0,
-      
-      // Top face
-      -1.0,  1.0, -1.0,
-      -1.0,  1.0,  1.0,
-       1.0,  1.0,  1.0,
-       1.0,  1.0, -1.0,
-      
-      // Bottom face
-      -1.0, -1.0, -1.0,
-       1.0, -1.0, -1.0,
-       1.0, -1.0,  1.0,
-      -1.0, -1.0,  1.0,
-      
-      // Right face
-       1.0, -1.0, -1.0,
-       1.0,  1.0, -1.0,
-       1.0,  1.0,  1.0,
-       1.0, -1.0,  1.0,
-      
-      // Left face
-      -1.0, -1.0, -1.0,
-      -1.0, -1.0,  1.0,
-      -1.0,  1.0,  1.0,
-      -1.0,  1.0, -1.0,
+      -SQUARE_RADIUS,  SQUARE_RADIUS,
+      SQUARE_RADIUS,  SQUARE_RADIUS,
+      -SQUARE_RADIUS, -SQUARE_RADIUS,
+      SQUARE_RADIUS, -SQUARE_RADIUS,
     ];
   
     // Now pass the list of positions into WebGL to build the
@@ -377,53 +330,20 @@ export class AppComponent {
                   new Float32Array(positions),
                   gl.STATIC_DRAW);
 
-    const faceColors = [
-      [1.0,  1.0,  1.0,  1.0],    // Front face: white
-      [1.0,  0.0,  0.0,  1.0],    // Back face: red
-      [0.0,  1.0,  0.0,  1.0],    // Top face: green
-      [0.0,  0.0,  1.0,  1.0],    // Bottom face: blue
-      [1.0,  1.0,  0.0,  1.0],    // Right face: yellow
-      [1.0,  0.0,  1.0,  1.0],    // Left face: purple
+    const colors = [
+      1.0,  1.0,  1.0,  1.0,    // white
+      1.0,  0.0,  0.0,  1.0,    // red
+      0.0,  1.0,  0.0,  1.0,    // green
+      0.0,  0.0,  1.0,  1.0,    // blue
     ];
-  
-    // Convert the array of colors into a table for all the vertices.
-    let colors = [];
-  
-    for (let j = 0; j < faceColors.length; ++j) {
-      const c = faceColors[j];
-  
-      // Repeat each color four times for the four vertices of the face
-      colors = colors.concat(c, c, c, c);
-    }
   
     const colorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-
-    const indexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-
-    // This array defines each face as two triangles, using the
-    // indices into the vertex array to specify each triangle's
-    // position.
-    const indices = [
-      0,  1,  2,      0,  2,  3,    // front
-      4,  5,  6,      4,  6,  7,    // back
-      8,  9,  10,     8,  10, 11,   // top
-      12, 13, 14,     12, 14, 15,   // bottom
-      16, 17, 18,     16, 18, 19,   // right
-      20, 21, 22,     20, 22, 23,   // left
-    ];
-
-    // Now send the element array to GL
-
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
-        new Uint16Array(indices), gl.STATIC_DRAW);
   
     return {
       position: positionBuffer,
       color: colorBuffer,
-      indices: indexBuffer,
     };
   }
 
@@ -482,12 +402,5 @@ function getRandomVelocity(): number[] {
   const theta = Math.random() * 2 * Math.PI;
   const xVel = Math.cos(theta) * .05
   const yVel = Math.sin(theta) * .05;
-  const zVel = Math.cos(Math.random() * 2 * Math.PI);
   return [xVel, yVel, 0.0];
-}
-
-function getRandomRotationAxis(): number[] {
-  const num = Math.random() * 2 * Math.PI;
-  const sin = Math.sin(num);
-  return [Math.cos(num), num < Math.PI ? sin : 0.0, num >= Math.PI ? sin : 0.0];
 }
